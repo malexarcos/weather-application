7404001d7e5a061c3ffbd20e2294d9bccf1267bb
malexarcos <110743192+malexarcos@users.noreply.github.com>
2022-12-04 14:30:27 -0600

    added some java code

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ var weatherApiRootUrl = 'https://api.openweathermap.org';
 var APIKey = "977e39b7fac766a952c34f79bfdf663f";
 
 var searchForm = document.querySelector
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var searchHistoryContainer = document.querySelector('#history');
+
+function renderSearchHistory(){
+    searchHistoryContainer.innerHTML = '';
+    for (var i = searchHistory.length - 1; >=0; 1--){
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append(btn);
+    }
+}
+
+function appendHistory(search){
+    if (searchHistory.indexOf(search) !== -1){
+        return;
+    }
+    searchHistory.push(search);
+    localStorage.setItem('search-history', JSON.stringify(searchHistory));
+    renderSearchHistory();
+}
